6316729110e528c6718e48f02bd7937b81dfab84
Luis Mera <luismera86@gmail.com>
2022-12-30 12:41:53 -0300

    Las 3 páginas de entrega envían la información al backend de forma local correctamente

--- src/pages/JavaScriptHomePage/components/JsPreDeliveryTow/JsPreDeliveryTow.tsx
+++ src/pages/JavaScriptHomePage/components/JsPreDeliveryTow/JsPreDeliveryTow.tsx
@@ -1,15 +1,31 @@
-import { Container, Grid, Paper, Typography } from '@mui/material'
+import { Button, Container, Grid, Paper, Typography } from '@mui/material'
 import { Evaluation, Feedback, Form } from '@/components'
+import { postData, setPreDeliveryNumber } from '@/redux/slices/dataSlice'
+import { useAppDispatch, useAppSelector } from '@/redux'
 
 import { jsDeliveryTow } from '@/data'
+import { useEffect } from 'react'
 import { useNote } from '@/hooks'
 
 export interface JsPreDeliveryTowInterface {}
 
 const JsPreDeliveryTow: React.FC<JsPreDeliveryTowInterface> = () => {
-  const { result, evaluationNoteOne, evaluationNoteTow, evaluationNoteThree, evaluationNoteFour, evaluationNoteFive } = useNote()
+  const dispatch = useAppDispatch()
+  const report = useAppSelector((state) => state.dataSlice)
+
+  const { result, evaluationNoteOne, evaluationNoteTow, evaluationNoteThree, evaluationNoteFour, evaluationNoteFive } =
+    useNote()
 
   const { evaluationOne, evaluationTow, evaluationThree, evaluationFour, evaluationFive } = jsDeliveryTow
+
+  useEffect(() => {
+    dispatch(setPreDeliveryNumber(2))
+  }, [])
+
+  const onHandleSendData = () => {
+    localStorage.setItem('studentName', '')
+    dispatch(postData(report))
+  }
 
   return (
     <Container sx={{ mt: 9, p: 4 }} maxWidth='lg'>
@@ -44,30 +60,35 @@ const JsPreDeliveryTow: React.FC<JsPreDeliveryTowInterface> = () => {
           </Grid>
         </Container>
         <Evaluation
+          evaluationNumber={1}
           title={evaluationOne.title}
           evaluation={evaluationOne.evaluation}
           evaluations={5}
           evaluationValue={evaluationNoteOne}
         />
         <Evaluation
+          evaluationNumber={2}
           title={evaluationTow.title}
           evaluation={evaluationTow.evaluation}
           evaluations={5}
           evaluationValue={evaluationNoteTow}
         />
         <Evaluation
+          evaluationNumber={3}
           title={evaluationThree.title}
           evaluation={evaluationThree.evaluation}
           evaluations={5}
           evaluationValue={evaluationNoteThree}
         />
         <Evaluation
+          evaluationNumber={4}
           title={evaluationFour.title}
           evaluation={evaluationFour.evaluation}
           evaluations={5}
           evaluationValue={evaluationNoteFour}
         />
         <Evaluation
+          evaluationNumber={5}
           title={evaluationFive.title}
           evaluation={evaluationFive.evaluation}
           evaluations={5}
@@ -77,6 +98,11 @@ const JsPreDeliveryTow: React.FC<JsPreDeliveryTowInterface> = () => {
           <Typography variant='h5'>Resultado: {result}</Typography>
         </Container>
         <Feedback />
+        <Container sx={{ mt: 3, textAlign: 'center' }}>
+          <Button variant='contained' size='large' onClick={onHandleSendData}>
+            Generar informe
+          </Button>
+        </Container>
       </Paper>
     </Container>
   )
