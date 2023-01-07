e37edb3904b17e79ac4d3fbc0ab116ac7a0345b1
Luis Mera <luismera86@gmail.com>
2023-01-06 21:24:59 -0300

    Modificación en guardado

--- src/components/ReportSaveButton.tsx
+++ src/components/ReportSaveButton.tsx
@@ -7,9 +7,8 @@ export const ReportSaveButton = () => {
   const dataSlice = useAppSelector((state) => state.dataSlice)
 
   const onHandleSaveHtml = () => {
-    const html = document.getElementsByTagName('html')[0].outerHTML
-    const css = document.getElementsByTagName('style')[0].innerHTML
-    const blob = new Blob([`<style>${css}</style>${html}`], { type: 'text/html;charset=utf-8' })
+    const html = document.documentElement.outerHTML
+    const blob = new Blob([html], { type: 'text/html' })
     saveAs(
       blob,
       `${dataSlice.student.name} - ${dataSlice.student.course} - ${dataSlice.student.commission} - ${dataSlice.preDeliveryNumber}.html`
